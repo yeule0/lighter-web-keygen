8c1afad9354de9baafa23c0bef839b2da1e1f9dd
zen <zhen0624@protonmail.com>
2025-09-11 01:03:02 +0000

    Fix TypeScript Buffer error for Netlify deployment

--- src/lib/wallet-vault-service.ts
+++ src/lib/wallet-vault-service.ts
@@ -1,6 +1,12 @@
 import { encrypt } from '@metamask/eth-sig-util'
 import { Buffer } from 'buffer'
 
+declare global {
+  interface Window {
+    Buffer: typeof Buffer
+  }
+}
+
 if (typeof window !== 'undefined' && !window.Buffer) {
   window.Buffer = Buffer
 }
